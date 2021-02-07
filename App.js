ff62d0356781b9dd3826b16b8d78763e7963f425
ochamug <hosea.natanael22@gmail.com>
2021-02-07 11:39:25 +0700

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,58 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import  React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, FlatList } from 'react-native';
 
 export default function App() {
+
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(()=>{
+    const temp = []
+    for(let i=0; i<20; i++){
+      temp.push({title: `Rapid Man ${i}`, id:`${i}`})
+    }
+    setBlogs(temp);
+  }, [])
+
+  const itemHandler = (id) => {
+    setBlogs(blogs.filter(blog => blog.id!=id));
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <FlatList 
+        data={blogs}
+        keyExtractor={(blog)=>blog.id}
+        style={{
+          width:"100%"
+        }}
+        renderItem={({item}) => (
+          <TouchableOpacity onPress={() => itemHandler(item.id)}>
+            <Text style={styles.item}>{item.title}</Text>  
+          </TouchableOpacity>
+        )}
+      />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ddd',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  item:{
+    marginTop: 30,
+    width: '100%',
+    padding: 30,
+    fontSize: 20,
+    fontWeight: 'bold',
+    backgroundColor: "#d0d",
+    color: "#fff",
+    borderRadius: 999,
+    marginHorizontal: 0,
+  }
 });
