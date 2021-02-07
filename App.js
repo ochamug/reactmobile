4785107c1639c24951ada66ab7050bfa2cc7e162
ochamug <hosea.natanael22@gmail.com>
2021-02-07 16:14:40 +0700

    ToDoApp Reset

--- App.js
+++ App.js
@@ -1,39 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
+
 import  React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, FlatList } from 'react-native';
 
 export default function App() {
 
-  const [blogs, setBlogs] = useState([]);
-
-  useEffect(()=>{
-    const temp = []
-    for(let i=0; i<20; i++){
-      temp.push({title: `Rapid Man ${i}`, id:`${i}`})
-    }
-    setBlogs(temp);
-  }, [])
-
-  const itemHandler = (id) => {
-    setBlogs(blogs.filter(blog => blog.id!=id));
-  }
-
-  return (
-    <View style={styles.container}>
-      <FlatList 
-        data={blogs}
-        keyExtractor={(blog)=>blog.id}
-        style={{
-          width:"100%"
-        }}
-        renderItem={({item}) => (
-          <TouchableOpacity onPress={() => itemHandler(item.id)}>
-            <Text style={styles.item}>{item.title}</Text>  
-          </TouchableOpacity>
-        )}
-      />
-    </View>
-  )
+  
 }
 
 const styles = StyleSheet.create({
